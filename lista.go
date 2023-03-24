9eec790a6b6cf15d9cffa4f0289dc3ac47021682
Julia Costa Carvalho <juliaacarvalhoo@dinfor.uniceub>
2023-03-24 10:37:09 -0300

    oii

---
+++ lista.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [3]int{1, 2, 3}
+
+	x := arr[0]
+	y := arr[1]
+	z := arr[2]
+
+	resultado := x + y + z
+	fmt.Println("A soma dos seus 3 números é igual a ", resultado)
+
+}
